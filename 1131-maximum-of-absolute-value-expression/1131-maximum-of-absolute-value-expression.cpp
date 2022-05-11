dde5c1fc71a45c59c9db393f963c057aa1766247
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-05-11 06:48:18 +0530

    Time: 62 ms (54.59%), Space: 24.4 MB (73.47%) - LeetHub

---
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
+            int maxans = 0;
+            int n = arr1.size();
+            int max1 = INT_MIN, min1 = INT_MAX;
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr1[i] + i + arr2[i];
+                max1 = max(max1, x);
+                min1 = min(min1, x);
+            }
+            maxans = max(maxans, abs(max1 - min1));
+            max1 = INT_MIN, min1 = INT_MAX;
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr1[i] - i + arr2[i];
+                max1 = max(max1, x);
+                min1 = min(min1, x);
+            }
+            maxans = max(maxans, abs(max1 - min1));
+            max1 = INT_MIN, min1 = INT_MAX;
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr1[i] + i - arr2[i];
+                max1 = max(max1, x);
+                min1 = min(min1, x);
+            }
+            maxans = max(maxans, abs(max1 - min1));
+            max1 = INT_MIN, min1 = INT_MAX;
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr1[i] - i - arr2[i];
+                max1 = max(max1, x);
+                min1 = min(min1, x);
+            }
+            maxans = max(maxans, abs(max1 - min1));
+            return maxans;
+        
+
+    }
+};
