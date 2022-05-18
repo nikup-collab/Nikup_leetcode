cbee83cf48ab397e5500ef11b85ce9ec606514b2
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-05-18 11:48:52 +0530

    Time: 47 ms (10.03%), Space: 23.2 MB (42.09%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start=0, end=s.size()-1;
+        while(start<end){
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+        
+    }
+};
