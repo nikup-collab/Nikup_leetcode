c65b715de5b5590bf2df95ae96418b2e2180cc05
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-01 11:50:47 +0530

    Time: 22 ms (73.74%), Space: 16.9 MB (53.47%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(vector<int>& nums) {  //xor with complete array gives the single array.
         int res=0;
         for(int i=0;i<nums.size();i++){
             res=res^nums[i];
