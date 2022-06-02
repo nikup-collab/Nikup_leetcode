f32e0b3016ba867b4ecfd2409238919090faa333
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-02 08:07:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.94%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count=0;
+        int count=0;           // every integer is right shift by inreasing and did "and" 1
         for(int i=0;i<32;i++){
             if(((n>>i) & 1) ==1)
                 count++;
