1d46a20833ef23c30bea68a607f128a65799c134
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-05 20:50:12 +0530

    Time: 10 ms (8.51%), Space: 7.9 MB (34.34%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>vect;
+        
+        for(int i=1;i<=n;i++){
+            if(i%15==0) vect.push_back("FizzBuzz");
+            else if(i%3==0) vect.push_back("Fizz");
+            else if(i%5==0)  vect.push_back("Buzz");
+            else vect.push_back(to_string(i));
+        }
+        return vect;
+        
+        
+    }
+};
