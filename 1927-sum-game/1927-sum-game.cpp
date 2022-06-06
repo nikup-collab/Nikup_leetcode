3465bcdc52fd5c76983e9f50bba331d608473560
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-06 09:54:00 +0530

    Time: 36 ms (43.65%), Space: 11.3 MB (83.98%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        double res = 0, n = num.length();
+        for (int i = 0; i < n; ++i)
+            res += (i < n / 2 ? 1 : -1) * (num[i] == '?' ? 4.5 : num[i] - '0');
+        return res != 0.0;
+    }
+};
