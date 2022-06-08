32fee83bf9ad51277dc1c0b85b2e449eb918d642
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-08 16:35:10 +0530

    Time: 2 ms (49.45%), Space: 6.3 MB (59.97%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,17 +1,66 @@
-#include<bits/stdc++.h>
-class Solution {
+class Solution
+{
 public:
-    int strStr(string haystack, string needle) {
-        if(needle.size()==0) return 0;
-        int n=haystack.size();
-        int m=needle.size();
-        
-        //brute force
-        for(int i=0;i<=n-m;i++){
-        int j=0;
-        while(j<m &&needle[j]==haystack[i+j]) j++;
-        
-        if(j==m) return i;
+    // function to generate the LPS array
+    vector<int>lpsGenerator(string &s)
+    {
+        int n=s.length(),i=1,j=0;
+        vector<int>lps(n);
+        lps[0]=0;
+        while(i<n)
+        {
+            if(s[i]==s[j])
+            {
+                lps[i]=j+1;
+                i++;
+                j++;
+            }
+            else
+            {
+                if(j>0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    lps[i++]=0;
+                }
+            }
+        }
+        return lps;
+    }
+    int strStr(string haystack, string needle)
+    {
+        if(needle.length()==0)
+        {
+            return 0;
+        }
+        vector<int>lps=lpsGenerator(needle);
+        int i=0,j=0,n=haystack.length(),m=needle.length();
+        // i is pointing to haystack
+        // j is pointing to needle
+        while(i<n)
+        {
+            if(needle[j]==haystack[i])
+            {
+                i++;
+                j++;
+            }
+            if(j==m)
+            {
+                return i-j;
+            }
+            else if(i<n && needle[j]!=haystack[i])
+            {
+                if(j>0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
         }
         return -1;
     }
