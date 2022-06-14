60787486dd3e138bec4b5bf636741246800c3a8a
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-14 10:23:25 +0530

    Time: 50 ms (7.85%), Space: 18 MB (19.11%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,20 +1,12 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        map<int,int> m;
-        int n=nums.size();
+        sort(nums.begin(),nums.end());
         
         for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+            if(nums[i]!=i) return i;
         }
-        int k=0;
-        for(auto v : m){
-            
-            if(v.first == k){
-                k++;
-            }
-            else return k;
+        return nums.size();
         }
-      return n;  
-    }
 };
