6167a3dcc4ecc81331a56603040f043d365f4b41
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-15 10:44:43 +0530

    Time: 3 ms (88.42%), Space: 12.4 MB (32.66%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+                int n= nums.size();
+        int pf[n];
+        int pfs[n];
+        pf[0]=nums[0];
+        pfs[n-1]=nums[n-1];
+        //prefix sum approach
+        for(int i=1;i<n;i++){
+            pf[i]=pf[i-1]+nums[i];
+            
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            pfs[i]=pfs[i+1]+nums[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            if(pf[i]==pfs[i]) return i;
+        }
+       return -1; 
+    }
+};
