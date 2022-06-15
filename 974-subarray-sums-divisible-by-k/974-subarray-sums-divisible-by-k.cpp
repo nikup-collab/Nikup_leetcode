8a70e8f542cbac8c779a164b07e5e6316e82d09b
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-15 19:49:04 +0530

    Time: 99 ms (33.15%), Space: 32.2 MB (6.44%) - LeetHub

---
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+       int n = nums.size();
+        long long pf[n+1];
+        
+        pf[0]=0;
+        
+        for(int i=1;i<=n;i++){
+            pf[i]=pf[i-1]+nums[i-1];
+            
+        }
+        
+        for(int i=0;i<=n;i++){
+            if(pf[i]>=0) pf[i]=pf[i]%k;
+            else{
+                pf[i]=(pf[i]- pf[i]*k)%k;
+            }
+        }
+        
+        map<int,int>m;
+        
+        for(int i=0;i<=n;i++){
+            m[pf[i]]++;
+        }
+        int count=0;
+        for(auto v: m){
+            int l=v.second;
+            count+= (l*(l-1))/2;
+            
+        }
+        return count;
+    }
+};
