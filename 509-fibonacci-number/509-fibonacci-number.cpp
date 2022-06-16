25171e52d096eb5d753381226aee434960a3b852
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-17 05:06:34 +0530

    Time: 3 ms (60.62%), Space: 6.4 MB (9.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,28 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
-    int fib(int n) {
+    int dpfib(int n, unordered_map<int,int> &lookup){
+        
+        //base case
         if(n==0) return 0;
         if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        
+        //if subproblem encountered first time calculate it
+        if(lookup.find(n) == lookup.end())
+            lookup[n]=  dpfib(n-1, lookup) + dpfib(n-2, lookup);
+        
+        return lookup[n];
+    }
+    
+    
+    
+    
+    int fib(int n) {
+        
+        //declare map  to store pre calculated valuees
+        unordered_map<int,int> lookup;
+        
+        return dpfib(n,lookup);
         
     }
 };
