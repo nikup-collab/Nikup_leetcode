d32ecad477a680e4920c3c3946e062581d46d455
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-20 22:11:10 +0530

    Time: 29 ms (5.12%), Space: 16.8 MB (5.63%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -3,6 +3,9 @@ public:
     bool isPrefixString(string s, vector<string>& words) {
         string str;
         
+        //i just create new string and check whether they are equal or not.
+        //transverse the complete array.
+        
         for(int i=0;i<words.size();i++){
             str=str+words[i];
              if(str==s) return true;
