5a76c256b74f696551de7f08bd833bb83ee78191
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-20 19:15:53 +0530

    Time: 3 ms (37.59%), Space: 6.1 MB (19.39%) - LeetHub

---
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.size()-1;
+        while (left < right) {
+            while (left < right && !isalpha(s[left])) left++;
+            while (left < right && !isalpha(s[right])) right--; 
+            swap(s[left++], s[right--]);
+        }
+        return s;
+    }
+};
