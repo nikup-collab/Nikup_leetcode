4c38af6702e0e5e12b1940103b5f167b87589cdb
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-23 16:39:28 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (8.97%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -3,22 +3,15 @@ public:
     vector<int> sumZero(int n) {
         vector<int> vect;
         
-        if(n%2==0){
+
             
             for(int i=1; i<=n/2; i++){
                 vect.push_back(i);
                 int num=0-i;
                 vect.push_back(num);
             }
-        }
-        else{
-            for(int i=1; i<=n/2; i++){
-                vect.push_back(i);
-                int num=0-i;
-                vect.push_back(num);
-            }
-            vect.push_back(0);
-        }
+        if(n%2!=0)  vect.push_back(0);
+        
         return vect;
     }
 };
