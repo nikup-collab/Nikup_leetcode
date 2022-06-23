d9515a5a91543f872dae967a370ed2bd43b1f48e
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-23 11:30:08 +0530

    Time: 212 ms (8.54%), Space: 16.7 MB (41.30%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -4,11 +4,11 @@ public:
         int ans=t.size();
         unordered_map<char,int> freq;
         
-        for(int i=0;i<t.size();i++){
+        for(int i=0;i<t.size();i++){ //frequency table of string t
             freq[t[i]]++;
         }
         int count=0;
-        for(int i=0; i<s.size();i++){
+        for(int i=0; i<s.size();i++){  //count frequency of character present in t
             if(freq.find(t[i])!=freq.end() && freq[s[i]]>0){
                 count++;
                 freq[s[i]]--;
@@ -17,3 +17,6 @@ public:
         return ans-count; 
     }
 };
+
+
+/* create a hashmap and count how many character of string already presemnt in string and reduce that*/
