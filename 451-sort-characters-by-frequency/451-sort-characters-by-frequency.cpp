a568b37a9c6e1f81093ce9e4a501dca99b55a82e
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-06-24 22:00:51 +0530

    Time: 23 ms (27.72%), Space: 16.2 MB (5.42%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -3,12 +3,12 @@ public:
     string frequencySort(string s) {
          unordered_map<char,int> m;
         
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<s.size();i++){  //creating a hash map of frequency char
             m[s[i]]++;
         }
         
         
-        vector<pair<int,char>> vect;
+        vector<pair<int,char>> vect;  //  tc: 'tree' vect: { 1,t ; 1,r : 2,e}
         
         for(auto v : m){
             vect.push_back({ v.second,v.first});
