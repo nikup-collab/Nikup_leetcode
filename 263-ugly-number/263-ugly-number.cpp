91e9e82ea8be7bb976663063d19dbd8fd857d932
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-02 17:18:26 +0530

    Time: 4 ms (40.25%), Space: 5.8 MB (86.47%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -6,9 +6,7 @@ public:
         while(n%3==0) n=n/3;
         while(n%5==0) n=n/5;
         
-        if(n==1) return true;
-        else
-            return false;
+       return n==1;
         
     }
 };
