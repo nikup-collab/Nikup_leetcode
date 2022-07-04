082703f7c768bfac43767b3ad96700332e2771eb
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-04 09:25:38 +0530

    Time: 15 ms (52.56%), Space: 7.3 MB (45.47%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,17 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
         
-        sort(s.begin(), s.end());
-        sort(t.begin(),t.end());
-        
-        int n=s.size();
-        int m=t.size();
-        
-        if(n!=m) return false;
-        
-        for(int i=0;i<n;i++){
-            if(s[i]!=t[i]) return false;
+        unordered_map<int,int>m;
+        for(int i=0;i<s.size(); i++){
+            m[s[i]]++;
+            m[t[i]]--;
         }
         
+        for(auto v: m){
+            if(v.second!=0) return false;
+        }
         return true;
     }
 };
