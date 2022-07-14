c7d204d503af8f3e2d6d459b8adc5889fc95f608
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-14 10:34:57 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (19.19%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,31 +1,34 @@
-#include<bits/stdc++.h>
+int tri(int n, vector<int> &dp){
+    
+    if(n==0) return 0;
+    if(n==1 || n==2) return 1;
+    //check if(dp[n] presoent ofr not
+    if(dp[n]!=-1) return dp[n];
+    
+    return dp[n]=tri(n-1,dp) +tri(n-2,dp) + tri(n-3,dp);
+    
+
+}
+
+
+
+
+
 class Solution {
 public:
-    int findtri(int n, unordered_map<int,int> &lookup){
-        //base case
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        
-        //if it look first time solve it
-        if(lookup.find(n) == lookup.end()){
-            lookup[n]=findtri(n-1, lookup) + findtri(n-2, lookup) + findtri(n-3,lookup);
-        }
-        
-        return lookup[n];
-    }
-    
-    
-    
-    
-    
-    
-    
     int tribonacci(int n) {
+//         //recursion
         
-        //declare map to check pre calculated valures
-        unordered_map<int,int> lookup;
+//         if(n==0) return 0;
+//         if(n==1 || n==2) return 1;
         
-        return findtri(n, lookup);
+//         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        
+        //memoization
+        
+        vector<int> dp(n+1,-1);
+        
+        return tri(n,dp);
         
     }
 };
