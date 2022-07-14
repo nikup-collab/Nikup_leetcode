f855837bf283bb1d158f1f8aa5ac23efec07c1ed
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-14 11:22:40 +0530

    Time: 4 ms (22.52%), Space: 6.2 MB (30.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,24 +1,34 @@
-#include<bits/stdc++.h>
+int climb(int n, vector<int> &dp){
+    
+    if(n==0) return 1;
+    if(n==1) return 1;
+    
+    //check if subproblem is oslved before or not
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    int stair(int n, unordered_map<int,int> &lookup ){
-        if(n==0 || n==1) return 1;
-        
-        //if it look first time calculate it
-        if(lookup.find(n)==lookup.end()){
-            lookup[n]= stair(n-1,lookup) + stair(n-2,lookup);
-        }
-        return lookup[n];
-    }
-    
-    
-    
-    
     int climbStairs(int n) {
-       //declared map to check pre calculated valuse
-        unordered_map<int,int> lookup;
         
-        return stair(n, lookup);
+        vector<int>dp(n+1,-1);
+        
+        return climb(n,dp);
+
         
     }
 };
