4b3466f285a21099bee621b4f908a55c78a27c3a
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-20 16:51:17 +0530

    Time: 7 ms (37.68%), Space: 8.3 MB (25.71%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,21 +1,19 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> m;
+        
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<arr.size(); i++) m[arr[i]]++;
+        
         vector<int> vect;
-        
-        for(int i=0;i<arr.size();i++){
-            m[arr[i]]++;
-        }
-        
         for(auto v: m){
             vect.push_back(v.second);
         }
         
-        sort(vect.begin(), vect.end());
+        sort(vect.begin(),vect.end());
         
-        for(int i=0;i<vect.size()-1; i++){
+        for(int i=0;i<vect.size()-1;i++){
             if(vect[i]==vect[i+1]) return false;
         }
         return true;
