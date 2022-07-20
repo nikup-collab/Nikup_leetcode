80ee4ae988174e78bc551244c98db4f036c42103
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-20 17:40:45 +0530

    Time: 179 ms (5.36%), Space: 32.9 MB (12.20%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        map<int,int>m;
+        
+        for(int i=1; i<=nums.size();i++) m[i]=0;
+        
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        
+        vector<int> vect(2,0);
+        
+        for(auto v: m){
+            
+            if(v.second==2) vect[0]=v.first;
+            if(v.second==0) vect[1]=v.first;
+        }
+        
+        return vect;
+    }
+    
+};
