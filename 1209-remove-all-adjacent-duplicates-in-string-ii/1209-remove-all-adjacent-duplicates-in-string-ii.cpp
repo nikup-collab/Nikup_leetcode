1aab63903be442dfed8b9a58acf8eeefee880c75
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-22 16:25:47 +0530

    Time: 77 ms (17.09%), Space: 10.7 MB (54.66%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -17,15 +17,10 @@ public:
             
             if(st.empty()|| st.top().first!=s[i]) st.push({s[i],1});
             else{
-                
-                
-                    auto prev =st.top();
-                    st.pop();
+                    auto prev =st.top(); st.pop();
                     st.push({s[i], prev.second+1});
                     
                     if(st.top().second==k)  st.pop();
-                
-
             }
         }
         string str;
