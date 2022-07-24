5a3c9584017e3f287921c59e14ccec9989390a95
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-24 10:16:04 +0530

    Time: 581 ms (6.94%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0;i<m;i++){              //loop run row time of matrix
-            int start=0, end=n-1;
-            while(start<=end){                //binary search algorithm to find target
-                int mid=(start+end)/2;
-                if(matrix[i][mid]==target)
-                    return true;
-                else if(matrix[i][mid]>target)
-                    end=mid-1;
-                else
-                    start=mid+1;
+        
+        for(int i=0;i<m;i++){
             
+            int lo=0, hi=n-1;
+            while(lo<=hi){
+                
+                int mid=lo+(hi-lo)/2;
+                if(matrix[i][mid]==target) return true;
+                
+                if(matrix[i][mid]>target) hi=mid-1;
+                else lo=mid+1;
             }
         }
         return false;
-        
     }
 };
