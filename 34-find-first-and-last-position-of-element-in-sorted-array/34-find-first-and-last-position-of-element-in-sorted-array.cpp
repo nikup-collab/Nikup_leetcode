9270399789223581e696e52127b2d57fe7ca930f
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-25 12:13:50 +0530

    Time: 8 ms (84.21%), Space: 13.8 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,52 +1,29 @@
 class Solution {
 public:
-
-    
-    int firstposition(vector<int> &nums, int target){
-        int res=-1;
-        int lo=0, hi=nums.size()-1;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            
-            if(nums[mid]==target){
-                res=mid;
-                hi=mid-1;
-                
-            } 
-            
-           else if(nums[mid]>target) hi=mid-1;
-            else lo=mid+1;
-        }
-        return res;
-    }
-    
-    
-    
-    int lastposition(vector<int> &nums, int target){
-        int r=-1;
-        int lo=0, hi=nums.size()-1;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            
-            if(nums[mid]==target){
-                r=mid;
-                lo=mid+1;
-            } 
-            
-           else  if(nums[mid]>target) hi=mid-1;
-            else lo=mid+1;
-        }
-        return r;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
         
-        v.push_back(firstposition(nums,target));
-        v.push_back(lastposition(nums,target));
+        vector<int>vect;
+        if(nums.size()==0){
+            vect.push_back(-1);
+            vect.push_back(-1);
+        }
         
-        return v;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                vect.push_back(i);
+                break;
+            }
+            if(i==nums.size()-1) vect.push_back(-1);
+        }
+        reverse(nums.begin(),nums.end());
         
-        
-        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                vect.push_back(nums.size()-i-1);
+                break;
+            }
+            if(i==nums.size()-1) vect.push_back(-1);
+        }
+        return vect;
     }
 };
