2eac47ae23b112c72b64c57e32201c021ee754ab
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-28 16:24:35 +0530

    Time: 15 ms (53.48%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        
-        unordered_map<int,int>m;
-        for(int i=0;i<s.size(); i++){
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++){
             m[s[i]]++;
             m[t[i]]--;
         }
