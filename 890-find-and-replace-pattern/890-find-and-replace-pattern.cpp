9d010706f465f502a4079179bb16b243f7a32f1f
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-29 08:59:48 +0530

    Time: 12 ms (18.71%), Space: 8.4 MB (65.33%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,26 +1,24 @@
-bool helper(string &s, string &t){
-    
-    if(s.size()!=t.size()) return false;
+bool help(string &s, string &s2){
     unordered_map<char,char>m;
-    for(int i=0;i<s.size();i++){
-        if(m.count(t[i])==1){
-            if(m[t[i]] != s[i]) return false;
-        } 
-
-        
-        m[t[i]]=s[i];
-        
-    }
-    vector<int> vect(26,0);
-    for(auto v:m){
-        vect[v.second-'a']++;
-    }
+    vector<char>vect;
+    if(s.size()!=s2.size()) return false;
     
-    for(auto v:vect){
+    for(int i=0;i<s.size();i++){
         
-        if(v>1) return false;
+        if(m.find(s2[i])!=m.end()){
+            if(m[s2[i]]!=s[i] )return false;
+        }
+        m[s2[i]]=s[i];
+    }
+    for(auto v: m){
+        vect.push_back(v.second);
+    }
+    sort(vect.begin(),vect.end());
+    for(int i=0;i<vect.size()-1;i++){
+        if(vect[i]==vect[i+1]) return false;
     }
     return true;
+     
 }
 
 
@@ -31,12 +29,16 @@ bool helper(string &s, string &t){
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+       
+        //for(int i=0;i<pattern.size();i++) m[pattern[i]]++;
+        
+        
         vector<string> ans;
         
         for(int i=0;i<words.size();i++){
-            
-            if(helper(words[i],pattern)==true) ans.push_back(words[i]);
+            if(help(words[i],pattern)==true) ans.push_back(words[i]);
         }
+        
         return ans;
     }
 };
