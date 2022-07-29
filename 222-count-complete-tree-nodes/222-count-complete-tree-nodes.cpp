746fbf20c8ef3d706c2a82384291a52086439d71
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-29 17:00:57 +0530

    Time: 54 ms (44.16%), Space: 30.9 MB (20.45%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -14,25 +14,7 @@ public:
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
         
-        int sum=0;
-        queue<TreeNode *>q;
         
-        q.push(root);
-        while(!q.empty()){
-            vector<int>level;
-            
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                
-                TreeNode *temp=q.front();
-                q.pop();
-                
-                if(temp->left) q.push(temp->left);
-                level.push_back(temp->val);
-                if(temp->right) q.push(temp->right);
-            }
-            sum+=level.size();
-        }
-        return sum;
+        return 1+ countNodes(root->left)+countNodes(root->right);
     }
 };
