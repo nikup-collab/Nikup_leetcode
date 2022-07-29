a94beefdb5a4ec22caf4f34ef07c629fdffcc2ad
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-07-29 09:02:11 +0530

    Time: 7 ms (55.39%), Space: 8.7 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,6 +1,7 @@
 bool help(string &s, string &s2){
     unordered_map<char,char>m;
-    vector<char>vect;
+    unordered_map<char,char>m2;
+    //vector<char>vect;
     if(s.size()!=s2.size()) return false;
     
     for(int i=0;i<s.size();i++){
@@ -8,15 +9,13 @@ bool help(string &s, string &s2){
         if(m.find(s2[i])!=m.end()){
             if(m[s2[i]]!=s[i] )return false;
         }
+        if(m2.find(s[i])!=m2.end()){
+            if(m2[s[i]]!=s2[i]) return false;
+        }
         m[s2[i]]=s[i];
+        m2[s[i]]=s2[i];
     }
-    for(auto v: m){
-        vect.push_back(v.second);
-    }
-    sort(vect.begin(),vect.end());
-    for(int i=0;i<vect.size()-1;i++){
-        if(vect[i]==vect[i+1]) return false;
-    }
+
     return true;
      
 }
