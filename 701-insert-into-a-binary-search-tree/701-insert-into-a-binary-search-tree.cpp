908e78f64cfe5a526fd25d0f08a5f29f8a367d30
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-08 21:50:05 +0530

    Time: 107 ms (85.43%), Space: 56.9 MB (49.14%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -13,15 +13,14 @@ class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         
-        if(root==NULL){
-            return new TreeNode(val);
-        }
+        if(root==NULL)   return new TreeNode(val);
+        
 //         if root->val is smaller than it will implemented into left
-        else if(val < root->val)
-                root->left = insertIntoBST(root->left, val);
+        else if(val < root->val)  root->left = insertIntoBST(root->left, val);
+        
+        
 //         if root->val is greatet than root it will implement to left
-         else
-                root->right = insertIntoBST(root->right, val);
+         else  root->right = insertIntoBST(root->right, val);
             
             
         
