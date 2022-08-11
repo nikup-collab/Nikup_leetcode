68d54070947f4eb372e63e2cb1cf77a620e6faee
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-11 23:01:35 +0530

    Time: 7 ms (20.98%), Space: 8.5 MB (10.42%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -5,11 +5,12 @@ public:
         vector<int>vect;
         
         for(int i=0;i<index.size();i++){
-            
+            // if index is equal to size push back the element to the vect
             if(vect.size()== index[i]){
                 vect.push_back(nums[i]);
                 
             }
+            // shift element to right and put the element in the given index
             else{
                 int n= vect.size();
                 vect.push_back(INT_MIN);
