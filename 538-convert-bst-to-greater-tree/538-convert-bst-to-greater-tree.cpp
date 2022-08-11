392e46844d9f0b76494323e40c599ceaf0b7093d
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-11 15:30:02 +0530

    Time: 70 ms (32.92%), Space: 33.5 MB (61.38%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -11,42 +11,14 @@
  */
 class Solution {
 public:
-    vector<int>vect;
-    unordered_map<int,int>mpp;
-    
-    TreeNode *constr(TreeNode *root, vector<int> &pf){
+  int curr_sum=0;
+    TreeNode* convertBST(TreeNode* root) {
         if(root==NULL) return NULL;
         
-        int i = mpp[root->val];
-        int sum = pf[vect.size()] - pf[i-1];
-        root->val= sum;
-        
-        constr(root->left, pf);
-        constr(root->right, pf);
+        convertBST(root->right);
+        root->val = (curr_sum += root->val);
+        convertBST(root->left);
         
         return root;
-    }
-    void help(TreeNode *root){
-        
-        if(root==NULL) return;
-        
-        help(root->left);
-        vect.push_back(root->val);
-        help(root->right);
-    }
-    TreeNode* convertBST(TreeNode* root) {
-        help(root);
-        int n= vect.size();
-        for(int i=0;i<vect.size() ; i++) mpp[vect[i]]=i+1;
-        
-        vector<int> pf(n+1,0);
-        
-        for(int i=1;i<=n;i++){
-            pf[i]= pf[i-1]+ vect[i-1];
-        }
-        
-        return constr(root, pf);
-        
-        
     }
 };
