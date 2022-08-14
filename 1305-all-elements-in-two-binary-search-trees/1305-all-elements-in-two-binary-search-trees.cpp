15c480c6effc57ad782caf310966a87b24032223
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-14 21:27:07 +0530

    Time: 227 ms (57.14%), Space: 85.2 MB (55.05%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,37 +11,22 @@
  */
 class Solution {
 public:
-    void help(TreeNode *root, vector<int> &vect){
+    vector<int>v;
+    void helper(TreeNode *r){
+        if(r==NULL) return;
         
-        if(root==NULL) return;
-        
-        help(root->left, vect);
-        vect.push_back(root->val);
-        help(root->right,vect);
+        helper(r->left);
+        v.push_back(r->val);
+        helper(r->right);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>v1;
-        vector<int>v2;
-        help(root1,v1);
-        help(root2,v2);
         
-        vector<int>answer;
+        helper(root1);
+        helper(root2);
         
-        int i=0,j=0;
-        while(i<v1.size() and j<v2.size()){
-            
-            if(v1[i]<v2[j]){
-                answer.push_back(v1[i]);
-                i++;
-            }
-            else{
-                answer.push_back(v2[j]);
-                j++;
-            }
-        }
-        for(int k=i;k<v1.size();k++) answer.push_back(v1[k]);
-        for(int l=j;l<v2.size();l++) answer.push_back(v2[l]);
+        sort(v.begin(), v.end());
         
-        return answer;
+        return v;
+        
     }
 };
