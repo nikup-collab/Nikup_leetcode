bafa7c9a58b4ce28cc98cc7ecf6183baa36b227e
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-25 09:53:27 +0530

    Time: 16 ms (62.32%), Space: 9.9 MB (89.39%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,20 +1,22 @@
-bool count(string &s,string &pref){
-    for(int i=0;i<pref.size();i++){
-        if(pref[i]!=s[i]) return false;
+int check(string &str, string &pref){
+    
+    for(int i=0;i<pref.size(); i++){
+        
+        if(pref[i]!=str[i]) return false;
     }
     return true;
 }
 
 
 
-
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int ans=0;
+       int count=0; 
         for(int i=0;i<words.size();i++){
-            if(count(words[i],pref) == true) ans++;
+            
+            if(check(words[i],pref)==true) count++;
         }
-        return ans;
+        return count;
     }
 };
