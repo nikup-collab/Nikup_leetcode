683eb53c6b33bc66e325744af63f198c67a65c57
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-25 10:20:30 +0530

    Time: 57 ms (5.68%), Space: 61.3 MB (5.04%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,22 +1,91 @@
-int check(string &str, string &pref){
+//declaring trie
+
+class TrieNode {
+    public:
+    bool isendword;
+    TrieNode *child[26];
+    int count;
     
-    for(int i=0;i<pref.size(); i++){
+    TrieNode() {
+        isendword=false;
         
-        if(pref[i]!=str[i]) return false;
+        for(int i=0;i<26;i++){
+            child[i]=NULL;
+        }
+        count=0;
     }
-    return true;
+    
+};
+
+//construct a trie
+
+void insert(TrieNode *root, string s){
+    
+    TrieNode *temp = root;
+    int k=0;
+    
+    for(int i=0;i<s.size();i++){
+        
+        k = s[i]-'a';
+        
+        if(temp->child[k]==NULL){
+            temp->child[k]= new TrieNode();
+        }
+        temp->child[k]->count++;
+        temp = temp->child[k];
+    }
+    temp->isendword=true;
 }
 
 
 
-class Solution {
-public:
-    int prefixCount(vector<string>& words, string pref) {
-       int count=0; 
-        for(int i=0;i<words.size();i++){
-            
-            if(check(words[i],pref)==true) count++;
-        }
-        return count;
+//check prefix
+
+int check(TrieNode *root, string pref){
+    
+    TrieNode *temp =root;
+    int k=0;
+    
+    for(int i=0;i<pref.size(); i++){
+        
+        k = pref[i]-'a';
+        
+        if(temp->child[k]==NULL) return 0;
+        
+        temp =temp->child[k];
     }
+    
+    return temp->count;
+}
+
+
+
+class Solution :public TrieNode{
+public:
+	int prefixCount(vector<string>& words, string pref) {
+		TrieNode* root= new TrieNode();
+		for(string word: words){
+			insert(root,word);
+		}
+		return check(root,pref);
+	}
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int prefixCount(vector<string>& words, string pref) {
+        
+//         TrieNode *root;
+        
+//         for(int i=0;i<words.size(); i++){
+//             insert(root, words[i]);
+//         }
+        
+//         return check(root, pref);
+        
+//     }
+// };
