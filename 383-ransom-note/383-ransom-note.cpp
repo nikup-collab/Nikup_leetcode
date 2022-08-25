eb30c0db8f660b9c362e1028b0539e7d3d663e9e
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-25 09:43:57 +0530

    Time: 15 ms (84.13%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        unordered_map<int,int>mpp;
+        vector<int> count(26,0);
         
-        for(int i=0;i<magazine.size();i++) mpp[magazine[i]]++;
+        for(auto ch : magazine){
+            count[ch-'a']++;
+        }
         
-        for(int i=0;i< ransomNote.size();i++){
-            
-            if(mpp.find(ransomNote[i])==mpp.end()) return false;
-            else{
-                mpp[ransomNote[i]]--;
-                if(mpp[ransomNote[i]]<0) return false;
-            }
+        for(auto ch : ransomNote){
+            if(count[ch-'a']-- <=0) return false;
         }
         return true;
     }
