5c7bc064cf0e451c3f94b58de5f6a784b60dcc23
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-08-25 11:18:54 +0530

    Time: 14 ms (56.17%), Space: 12.2 MB (11.65%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,19 +1,64 @@
-bool check(string st, string s){
-    for(int i=0;i<st.size();i++){
-        if(st[i]!=s[i]) return false;
+// declarng a trie
+class trienode {
+    public:
+    bool isendword;
+    trienode *child[26];
+    trienode() {
+        isendword=false;
+        
+        for(int i=0;i<26;i++){
+            child[i]=NULL;
+        }
+    }
+};
+
+
+void insert(trienode *root, string s){
+    trienode *curr = root;
+    int k=0;
+    
+    for(int i=0;i<s.size(); i++){
+        k= s[i]-'a';
+        
+        if(curr->child[k]==NULL) {
+            curr->child[k]= new trienode();
+        }
+        curr= curr->child[k];
+    }
+    curr->isendword=true;
+}
+
+bool check(trienode *root, string s){
+    
+    trienode *curr=root;
+    int k=0;
+    
+    for(int i=0;i<s.size(); i++){
+        k=s[i]-'a';
+        
+        if(curr->child[k]==NULL) return false;
+        
+        curr= curr->child[k];
     }
     return true;
 }
 
 
 
-class Solution {
+
+
+
+class Solution  : public trienode 
+{
 public:
+    trienode *root;
     int countPrefixes(vector<string>& words, string s) {
-        
+    root= new trienode();
+        insert(root, s);
         int count=0;
         for(int i=0;i<words.size();i++){
-            if(check(words[i],s)==true) count++;
+            
+            if(check(root, words[i])==true) count++;
         }
         return count;
     }
