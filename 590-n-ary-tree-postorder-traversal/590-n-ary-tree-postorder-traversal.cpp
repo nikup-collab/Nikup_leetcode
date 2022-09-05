c268cf869066dd3a5215243739ef2fa2ed9671f8
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-09-05 13:22:25 +0530

    Time: 54 ms (12.32%), Space: 11.4 MB (29.59%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int>answer;
+    
+    void help(Node *root){
+        
+        if(root==NULL) return;
+        
+        
+        
+        for(int i=0;i<root->children.size();i++){
+            help(root->children[i]);
+        }
+        answer.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        if(root==NULL) return answer;
+        
+        help(root);
+        
+        return answer;
+        
+    
+        
+    }
+};
