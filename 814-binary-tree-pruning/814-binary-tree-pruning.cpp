7c5bb48adb0e5bc4c9af52c62c67c38ce3f2b3b5
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-09-06 16:30:37 +0530

    Time: 13 ms (6.47%), Space: 8.8 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -11,18 +11,15 @@
  */
 class Solution {
 public:
-
-
     TreeNode* pruneTree(TreeNode* root) {
+        
         if(root==NULL) return NULL;
-    
         
-       root->left= pruneTree(root->left);
-       root->right=  pruneTree(root->right);
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
         
-        if(root->val==0 && root->left ==NULL && root->right==NULL) return NULL;
-
+        if(root->val ==0 and root->left == NULL and root->right == NULL) return NULL;
         
-       return root;
+        return root;
     }
 };
