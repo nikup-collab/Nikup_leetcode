dd038bdb268b181b5b1aa9d209bfbf67305d9aef
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-09-29 13:41:02 +0530

    Time: 3 ms (37.77%), Space: 6 MB (26.20%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -12,9 +12,7 @@ public:
             n=n>>1;
         }
         
-        // for(int i=0;i<vect.size()-1;i++){
-        //     if(vect[i]== vect[i+1]) return false;
-        // }
+        
         return true;
     }
 };
