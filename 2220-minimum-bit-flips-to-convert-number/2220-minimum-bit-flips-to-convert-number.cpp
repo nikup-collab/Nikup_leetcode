a62528b233f4743443c0a6a295c392b7a357a9e5
nikup-collab <70276604+nikup-collab@users.noreply.github.com>
2022-09-29 17:03:03 +0530

    Time: 3 ms (44.19%), Space: 5.8 MB (95.08%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -2,39 +2,13 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         
-        vector<int>arr;
-        vector<int>vect;
-        
-        while(start){
-            arr.push_back(start%2);
-            
-            start = start>>1;
-        }
-        
-         while(goal){
-            vect.push_back(goal%2);
-            
-            goal = goal>>1;
-        }
+        int c = start^goal;
         int count=0;
-        int n = min(arr.size(), vect.size());
-        int i;
-        for(i=0;i<n;i++){
+        while(c){
+             if(c%2==1) count++;
             
-            if(arr[i] != vect[i]) count++;
-            }
-        while(i<arr.size()){
-            if(arr[i]==1) count++;
-            i++;
+            c=c>>1;
         }
-        while(i<vect.size()){
-            if(vect[i]==1) count++;
-            i++;
-            
-        }
-        
-            
         return count;
-        
     }
 };
